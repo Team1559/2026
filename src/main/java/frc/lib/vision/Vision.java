97fd24e4a4c686ea8c1cddfd7bcece57adc2e48d
SWDevilTech <swdeviltech@gmail.com>
2026-02-20 15:43:27 -0500

    Updated and tested new standard deviations in preparation for merge into main

--- src/main/java/frc/lib/vision/Vision.java
+++ src/main/java/frc/lib/vision/Vision.java
@@ -2,29 +2,27 @@ package frc.lib.vision;
 
 import static edu.wpi.first.units.Units.Inches;
 import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Radians;
 
-import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
 import org.littletonrobotics.junction.Logger;
 
-import com.fasterxml.jackson.databind.deser.impl.PropertyBasedObjectIdGenerator;
-
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.math.VecBuilder;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.DriverStation;
 import frc.lib.LoggableSubsystem;
 import frc.lib.vision.VisionCameraIo.PoseObservation;
 import frc.lib.vision.VisionCameraIo.PoseObservationType;
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.wpilibj.DriverStation;
 
 public class Vision extends LoggableSubsystem {
-    private static final double linearStdDevBaseline = 0.1; // TODO: set constants
-    private static final double angularStdDevBaseline = 0.1;
+    private static final double linearStdDevBaseline = 0.2;
+    private static final double angularStdDevBaseline = 0.2;
     private static final double linearStdDevMegatag2Factor = 1;
     private static final double angularStdDevMegatag2Factor = Double.POSITIVE_INFINITY;
     private static final double maxAmbiguity = 0.1;
@@ -32,8 +30,6 @@ public class Vision extends LoggableSubsystem {
     private final VisionComponent[] cameras;
     private final VisionConsumer visionConsumer;
     private final AprilTagFieldLayout aprilTagLayout;
-
-    private boolean testbool = false;
 
     public Vision(String name, VisionConsumer visionConsumer, AprilTagFieldLayout aprilTagLayout,
             VisionComponent... cameras) {
@@ -50,10 +46,10 @@ public class Vision extends LoggableSubsystem {
         boolean robotHasPose = false;
 
         List<Pose3d> tagPoses = new LinkedList<>();
-        
+
         List<RejectedObservation> rejectedObservations = new LinkedList<>();
-        List<PoseObservation> acceptedObservations = new LinkedList<>();
-        
+        List<AcceptedObservation> acceptedObservations = new LinkedList<>();
+
         for (VisionComponent cam : cameras) {
             for (int tagId : cam.getTagIds()) {
                 Optional<Pose3d> tagPose = aprilTagLayout.getTagPose(tagId);
@@ -61,55 +57,49 @@ public class Vision extends LoggableSubsystem {
                     tagPoses.add(tagPose.get());
                 }
             }
-            
+
             for (PoseObservation observation : cam.getPoseObservations()) {
-                // boolean rejectPose = observation.tagCount() == 0 // Must have at least one
-                // tag
-                // || (observation.tagCount() == 1 && observation.ambiguity() > maxAmbiguity)
-                // || Math.abs(observation.pose().getZ()) > maxZError.in(Meters) // TODO: pass
-                // in max z error
-                // || (observation.type() == PoseObservationType.MEGATAG_2 &&
-                // DriverStation.isDisabled())
-                
-                // // Must be within the field boundaries
-                // || observation.pose().getX() < 0.0
-                // || observation.pose().getX() > aprilTagLayout.getFieldLength()
-                // || observation.pose().getY() < 0.0
-                // || observation.pose().getY() > aprilTagLayout.getFieldWidth();
-                
                 if (observation.tagCount() == 0) {
                     rejectedObservations.add(new RejectedObservation(observation, "No tags"));
                     continue;
                 }
+
                 if (observation.tagCount() == 1 && observation.ambiguity() > maxAmbiguity) {
                     rejectedObservations.add(new RejectedObservation(observation, "One tag, too ambiguous"));
                     continue;
                 }
+
+                if (observation.pose().getZ() > maxZError.in(Meters)) {
+                    rejectedObservations.add(new RejectedObservation(observation, "Large Z error"));
+                    continue;
+                }
+
                 if (observation.type() == PoseObservationType.MEGATAG_2 && DriverStation.isDisabled()) {
                     rejectedObservations.add(new RejectedObservation(observation, "Megatag 2 and disabled"));
                     continue;
                 }
+
                 if (observation.pose().getX() < 0.0
-                || observation.pose().getX() > aprilTagLayout.getFieldLength()
-                || observation.pose().getY() < 0.0
-                || observation.pose().getY() > aprilTagLayout.getFieldWidth()) {
+                        || observation.pose().getX() > aprilTagLayout.getFieldLength()
+                        || observation.pose().getY() < 0.0
+                        || observation.pose().getY() > aprilTagLayout.getFieldWidth()) {
                     rejectedObservations.add(new RejectedObservation(observation, "Outside of field"));
                     continue;
                 }
-                
-                acceptedObservations.add(observation);
-                
+
                 double stdDevFactor = Math.pow(observation.averageTagDistance(), 2.0) / observation.tagCount();
                 double linearStdDev = linearStdDevBaseline * stdDevFactor;
                 double angularStdDev = angularStdDevBaseline * stdDevFactor;
-                
+
                 if (observation.type() == PoseObservationType.MEGATAG_2) {
                     linearStdDev *= linearStdDevMegatag2Factor;
                     angularStdDev *= angularStdDevMegatag2Factor;
                 }
-                
+
+                acceptedObservations.add(new AcceptedObservation(observation, Meters.of(linearStdDev), Radians.of(angularStdDev)));
+
                 visionConsumer.addVisionMeasurement(observation.pose().toPose2d(), observation.timestamp(),
-                VecBuilder.fill(linearStdDev, linearStdDev, angularStdDev));
+                        VecBuilder.fill(linearStdDev, linearStdDev, angularStdDev));
                 robotHasPose = true;
             }
         }
@@ -117,26 +107,32 @@ public class Vision extends LoggableSubsystem {
         List<Pose3d> robotPosesAccepted = new LinkedList<>();
         List<Pose3d> robotPosesRejected = new LinkedList<>();
 
-        for (RejectedObservation observation :rejectedObservations) {
+        for (RejectedObservation observation : rejectedObservations) {
             robotPosesRejected.add(observation.observation.pose());
         }
 
-        for (PoseObservation observation : acceptedObservations) {
-            robotPosesAccepted.add(observation.pose());
+        for (AcceptedObservation observation : acceptedObservations) {
+            robotPosesAccepted.add(observation.observation.pose());
         }
-        
+
         Logger.recordOutput(getOutputLogPath("HasVisionRead"), robotHasPose);
         Logger.recordOutput(getOutputLogPath("TagPoses"), tagPoses.toArray(Pose3d[]::new));
         Logger.recordOutput(getOutputLogPath("RobotPosesAccepted"), robotPosesAccepted.toArray(Pose3d[]::new));
         Logger.recordOutput(getOutputLogPath("RobotPosesRejected"), robotPosesRejected.toArray(Pose3d[]::new));
-        
-        Logger.recordOutput(getOutputLogPath("RejectedObservations"), rejectedObservations.toArray(RejectedObservation[]::new));
-        Logger.recordOutput(getOutputLogPath("AcceptedObservations"), acceptedObservations.toArray(PoseObservation[]::new));
-        
+
+        Logger.recordOutput(getOutputLogPath("RejectedObservations"),
+                rejectedObservations.toArray(RejectedObservation[]::new));
+        Logger.recordOutput(getOutputLogPath("AcceptedObservations"),
+                acceptedObservations.toArray(AcceptedObservation[]::new));
+
     }
 
     public record RejectedObservation(
             PoseObservation observation,
-            String reason) {
-    }
+            String reason) {}
+
+    public record AcceptedObservation(
+            PoseObservation observation,
+            Distance linearStdDev,
+            Angle angularStdDev) {}
 }
